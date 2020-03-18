5dd8c8e4f727337ad7c964266b430e20cfaaa068
maxilex <61802609+maxilex@users.noreply.github.com>
2020-03-18 15:39:49 +0400

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,6 +1,6 @@
 // firebase-messaging-sw.js
-importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/7.11.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/7.11.0/firebase-messaging.js');
 
 firebase.initializeApp({
     messagingSenderId: '562339649853'
