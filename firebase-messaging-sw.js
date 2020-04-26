cbe5087821422f0dba2bf7fe3e60fd6d36ca9203
maxilex <61802609+maxilex@users.noreply.github.com>
2020-04-26 23:40:25 +0400

    Delete firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++
@@ -1,9 +0,0 @@
-// firebase-messaging-sw.js
-importScripts('https://www.gstatic.com/firebasejs/7.11.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/7.11.0/firebase-messaging.js');
-
-firebase.initializeApp({
-    messagingSenderId: '562339649853'
-});
-
-const messaging = firebase.messaging();
